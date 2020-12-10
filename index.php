23c1f2a87f96bc60849e8634a977e3cbdad24824
Ramil <zarram89@gmail.com>
2020-12-10 22:44:29 +0500

    Убираем лишнюю задачу

--- index.php
+++ index.php
@@ -150,19 +150,6 @@ $tasks = [
                             <td class="task__date"></td>
                         </tr>
                     <?php endforeach; ?>
-
-                    <?php if($show_complete_tasks == 1) : ?>
-                    <tr class="tasks__item task task--completed">
-                        <td class="task__select">
-                            <label   label class="checkbox task__checkbox">
-                                <input class="checkbox__input visually-hidden" type="checkbox" checked>
-                                <span class="checkbox__text">Записаться на интенсив "Базовый PHP"</span>
-                            </label>
-                        </td>
-                        <td class="task__date">10.10.2019</td>
-                        <td class="task__controls"></td>
-                    </tr>
-                    <?php endif; ?>
                 </table>
             </main>
         </div>
